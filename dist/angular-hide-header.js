9bfda535bce85cc83ec82cd1658fca2d3ae47871
chirilaionut <chirila.ionut87@gmail.com>
2016-05-18 19:10:30 +0300

    Changed module for personal use

--- dist/angular-hide-header.js
+++ dist/angular-hide-header.js
@@ -13,22 +13,34 @@
 					var pxOffset = parseInt(hideOffset);
 					$timeout.cancel(scroll_time);
 					if(current_scroll >= hheight+pxOffset){
-						if (current_scroll < scrollposition) {
-							element.css({'top':"0px"});
+						if (current_scroll <= scrollposition) {
+					 		element.removeClass('hideh');
+							element.css({'top':"80px"});
+							angular.element('#sf-fixed-left-menu').css({'top': "164px"});
 						}
-						else if (current_scroll > scrollposition) {
+						else{
+					  		element.addClass('hideh');
+							console.log(hheight);
 							element.css({
 								'top':-hheight+"px",
-								'transition':'all 0.25s',
-								'-webkit-transition': 'all 0.25s',
-								'-moz-transition': 'all 0.25s',
-								'-ms-transition': 'all 0.25s',
-								'-o-transition': 'all 0.25s',
+								'transition':'top 0.25s',
+								'-webkit-transition': 'top 0.25s',
+								'-moz-transition': 'top 0.25s',
+								'-ms-transition': 'top 0.25s',
+								'-o-transition': 'top 0.25s',
 							});
+							angular.element('#sf-fixed-left-menu').css({'top': "84px"});
 						}
+					}
+					else if(current_scroll >= hheight){
+						element.removeClass('hideh');
+						element.css({
+							'top':"80px"});
+						angular.element('#sf-fixed-left-menu').css({'top': "164px"});
 					}
 					scroll_time = $timeout(function(){
 						scrollposition = body[0].scrollTop;
+						console.log(scrollposition);
 					},60);
 				});
 			}
